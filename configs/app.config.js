29c90b114b7857bc3abce86f1f3fd9a0f9a7c3ad
BBboy01 <1156678002@qq.com>
2021-08-30 18:41:21 +0800

    .

--- configs/app.config.js
+++ configs/app.config.js
@@ -1,14 +1,7 @@
 const Koa = require("koa");
-const session = require("koa-session");
-const static = require("koa-static");
 const bodyParser = require("koa-bodyparser");
-const formidable = require("koa2-formidable");
-const koaRouter = require("koa-router")();
-const path = require("path");
 
-const noteRouter = require("../routers/note.router");
-const categoryRouter = require("../routers/category.router");
-const authRouter = require("../routers/auth.router");
+const useRoutes = require("../routers");
 const { handleError, handle404 } = require("../utils/handleExceptions");
 const { accessLogger } = require("../utils/logger");
 const errorHandler = require("../utils/errorHandler");
@@ -23,19 +16,14 @@ app
     ctx.set("Access-Control-Allow-Origin", "*");
     await next();
   })
-  .use(static(path.join(__dirname, "public")))
-  .use(session({ maxAge: 1000 * 3600 }, app))
-  .use(formidable())
   .use(
     bodyParser({
       enableTypes: ["json", "form", "text"],
     })
   )
   .use(handleError)
-  .use(handle404)
-  .use(noteRouter.routes())
-  .use(authRouter.routes())
-  .use(categoryRouter.routes())
-  .use(koaRouter.allowedMethods()); // according to 'ctx.status' to set response header of 'response'
+  .use(handle404);
+
+useRoutes(app);
 
 module.exports = app;
